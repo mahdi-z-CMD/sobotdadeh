4a4f9e4ba8df58e5cda7cd1468940ab49243110f
M Z <mahdi.zxzxz@gmail.com>
2024-02-25 18:44:53 +0330

    home page end

--- src/Home.jsx
+++ src/Home.jsx
@@ -17,18 +17,36 @@ import masir1icon from './Icons/masir1.svg'
 import masir2icon from './Icons/masir2.svg'
 import masir3icon from './Icons/masir3.svg'
 import arrowmasiricon from './Icons/arrowmasir.svg'
+import expanddownsoalat from './Icons/expanddownsoalat.svg'
+import expandmoresolat from './Icons/expandmoresolat.svg'
+import arrowback from './Icons/arrowback.svg'
+import aparaticon from './Icons/aparaticon.svg'
+import telegramicon from './Icons/telegramicon.svg'
+import instagramicon from './Icons/instagramicon.svg'
 // images
 import boximage1 from './image/box1.png' 
 import boximage2 from './image/box2.png' 
 import scrolldownicon from './image/scrolldown.png'
 import majaleimg1 from './image/majale1.png' 
 import majaleimg2 from './image/majale2.png' 
-import majaleimg3 from './image/majale3.png' 
+import majaleimg3 from './image/majale3.png'
+import namadimage1 from './image/namad1.png'
+import namadimage2 from './image/namad2.png'
+import namadimage3 from './image/namad3.png'
+import foterimg from './image/foter.svg' 
 
 // json test for api
 import sliderdata from './slidersdata.json'
 
 export const Homepage = () => {
+  // this section is for box soalat 
+  const [expandedIndex, setExpandedIndex] = useState(-1);
+
+  const handleBoxClick = (index) => {
+    setExpandedIndex(prevIndex => (prevIndex === index ? -1 : index));
+  };  
+  // this section is for box soalat 
+
   // thie section is for box khadamat animation 
   const [scrolled, setScrolled] = useState(false);
   const [majale, setMajale] = useState({
@@ -254,8 +272,110 @@ export const Homepage = () => {
             </div>
           </div>
       </div>
-    <div className="soalat">
-
+      <div className="soalat">
+      <h1>سوالات متداول</h1>
+      <div className={`box-soalat ${expandedIndex === 0 ? 'active' : ''}`}>
+        <div
+          className="box-soalat-header"
+          onClick={() => handleBoxClick(0)}
+        >
+          <h2>آیا برای درخواست کار نیاز به رزومه دارم؟</h2>
+          <img src={expandedIndex === 0 ? expandmoresolat : expanddownsoalat} alt="expand down" />
+        </div>
+        {expandedIndex === 0 && (
+          <div className="additional-text">
+            <img src={arrowback} alt="arrow back" />
+            <p>بله تمامی شرکت‌هایی که در ثبات‌داده آگهی ثبت میکنند شرکت‌های ثبت شده هستند.</p>
+          </div>
+        )}
+      </div>
+      <div className={`box-soalat ${expandedIndex === 1 ? 'active' : ''}`}>
+        <div
+          className="box-soalat-header"
+          onClick={() => handleBoxClick(1)}
+        >
+          <h2>برای ثبت شرکت نیاز به تاییدیه هست؟</h2>
+          <img src={expandedIndex === 1 ? expandmoresolat : expanddownsoalat} alt="expand down" />
+        </div>
+        {expandedIndex === 1 && (
+          <div className="additional-text">
+            <img src={arrowback} alt="arrow back" />
+            <p>بله تمامی شرکت‌هایی که در ثبات‌داده آگهی ثبت میکنند شرکت‌های ثبت شده هستند.</p>
+          </div>
+        )}
+      </div>
+      <div className={`box-soalat ${expandedIndex === 2 ? 'active' : ''}`}>
+        <div
+          className="box-soalat-header"
+          onClick={() => handleBoxClick(2)}
+        >
+          <h2>چگونه میتوانیم از جدیدترین آگهی‌های استخدام با خبر شوم؟</h2>
+          <img src={expandedIndex === 2 ? expandmoresolat : expanddownsoalat} alt="expand down" />
+        </div>
+        {expandedIndex === 2 && (
+          <div className="additional-text">
+            <img src={arrowback} alt="arrow back" />
+            <p>بله تمامی شرکت‌هایی که در ثبات‌داده آگهی ثبت میکنند شرکت‌های ثبت شده هستند.</p>
+          </div>
+        )}
+      </div>
+    </div>
+    <div className="footer">
+      <img src={foterimg} alt="back ground" className='footerbg'/>
+      <div className="footet-detail">
+        <div className="signup">
+          <h1>برای استفاده از امکانات ثبات داده، ثبت‌نام کنید</h1>
+          <div className="singup-submit">
+            <input type="email" name="email" placeholder='پست الکترونیکی را وارد نمایید ...'/>
+            <button type="submit">ثبت درخواست</button>
+          </div>
+        </div>
+        <div className="footer-about">
+          <h2>ثبات داده، پلتفرمی برای تمامی شرکت ها</h2>
+          <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد</p>
+        </div>
+        <h1>دسترسی سریع</h1>
+        <div className="footer-pages">
+          <div className="footer-namads">
+            <div className="namads-place">
+              <img src={namadimage1} alt="e namad" width="112px" height="112px"/>
+            </div>
+            <div className="namads-place">
+              <img src={namadimage2} alt="e namad" width="90px" height="90px"/>
+            </div>
+            <div className="namads-place">
+              <img src={namadimage3} alt="e namad" width="112px" height="112px"/>
+            </div>
+          </div>
+          <div className="links">
+              <div className="links-row">
+                <a>سوالات متداول</a>
+                <a>پشتیبانی</a>
+                <a>درباره ثبات‌داده</a>
+              </div>
+              <div className="links-row">
+                <a>ثبت آگهی شغلی</a>
+                <a>قوانین</a>
+                <a>ارتباط با ما</a>
+              </div>
+              <div className="links-row">
+                <a>دانلود اپلیکیشن</a>
+                <a>بلاگ</a>
+              </div>
+            </div>
+        </div>
+        <hr />
+        <div className="footer-end">
+          <div className="social-icon">
+            <img src={aparaticon} alt="aparat icon" width="24px" height="24px"/>
+            <img src={instagramicon} alt="instagram icon" width="24px" height="24px"/>
+            <img src={telegramicon} alt="telegram icon" width="24px" height="24px"/>
+          </div>
+          <div className="hoghogh">
+            <h1>تمامی حقوق این سایت متعلق به سایت sobotdadeh می‌باشد.</h1>
+          </div>
+        </div>
+      </div>
     </div>
 	</div>
   )
